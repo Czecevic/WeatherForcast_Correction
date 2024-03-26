16c411e22137fab2a44a72bb864dd188e5303225
Charles <charleszecevic@gmail.com>
2024-03-26 15:55:02 +0100

    change API

--- index.js
+++ index.js
@@ -1,83 +1,103 @@
+// API Keys
 const API_KEY_OPW = "e609c5aa34dce05160444d30a165ee10";
-const API_KEY_OCD = "3456114c599c400fbcbafa7852f29bc4";
+const API_KEY_OCD = "93d0797359da49bf830dc5d10f58197b";
 
-function ocd(arg) {
-  return fetch(
-    `https://api.opencagedata.com/geocode/v1/json?q=${arg}&key=${API_KEY_OCD}&pretty=1&limit=1`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      return data;
-    })
-    .catch((error) => console.log(error));
-}
+// declaration
+const city = document.querySelector("#form-meteo");
+const nameOfCity = document.querySelector("#city");
+const eltSelect = document.querySelector("select");
 
-function opw(arg, id) {
-  return fetch(
-    `https://api.openweathermap.org/data/2.5/onecall?lat=${arg[0].lat}&lon=${arg[0].lng}&cnt=7&exclude=minutely,hourly&units=metric&appid=${API_KEY_OPW}`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      let hour = new Date(data.daily[0].dt * 1000)
-      console.log(hour)
-      for (let i = 0; i < id; i++) {
-        getWeather(data.daily[i].weather[0].id, data.daily[i].dt);
-      }
-      if (hour.getHours() >= 18 || hour.getHours() <= 8) {
-        document.querySelector("body").style.backgroundColor = "#1f263b";
-        document.querySelectorAll('*').forEach((node) => {
-          node.style.color = "#fff"
-        })
-        document.querySelectorAll('img').forEach(element => {
-          element.style.filter = "invert(1)"
-        });
-      } else {
-        document.querySelector("body").style.backgroundColor = "#bcd4e6";
-      }
-    });
-}
+// call API
+const openCageData = async (city) => {
+  const reponse = await fetch(`
+    https://api.opencagedata.com/geocode/v1/json?q=${city}&key=${API_KEY_OCD}
+    `);
+  const data = await reponse.json();
+  return data;
+};
 
-function getWeather(id, hours) {
+const openWeatherMap = async (position) => {
+  const reponse = await fetch(`
+    https://api.openweathermap.org/data/2.5/forecast?lat=${position.lat}&lon=${position.lng}&appid=${API_KEY_OPW}
+    `);
+  const data = await reponse.json();
+  return data;
+};
+
+// temps - meteo
+const getWeather = (id, hours) => {
   let path;
-  let hour = new Date(hours * 1000);
+  let day = new Date(hours * 1000);
   if (id === 800) {
-    path = "./assets/sun.svg";
-  } else if (id >= 600 && 622 >= id) {
-    path = "./assets/snow.svg";
-  } else if (id >= 801 && 804 >= id) {
-    if (id === 801 || id === 802) {
-      path = "./assets/cloudy.svg";
-    }
-    if (id === 803 || id === 804) {
-      path = "./assets/clouds.svg";
-    }
+    path = "../assets/sun.svg";
+  } else if (id === 801 || id === 802) {
+    path = "../assets/cloudy.svg";
+  } else if (id === 803 || id === 804) {
+    path = "../assets/clouds.svg";
+  } else if (id > 600 && id < 622) {
+    path = "../assets/snow.svg";
   } else {
-    path = "./assets/rain.svg";
+    path = "../assets/rain.svg";
   }
   document.querySelector("#container").innerHTML += `
-    <div class="card-day" data-id=${id}>
-    <h1>${hour.toLocaleDateString("fr-FR", { weekday: "long" })}</h1>
-        <img id="imageURL" class="card-icon" src="${path}" alt="weather image">
+    <div class="card-day">
+        <h1> ${day.toLocaleDateString("fr-FR", { weekday: "long" })}</h1>
+        <img class="card-icon" src="${path}" alt="weather icon">
     </div>
-    `;
-}
+  `;
+};
 
-document.addEventListener("DOMContentLoaded", () => {
-  let date = new Date();
-  document.getElementById("form-meteo").addEventListener("submit", (event) => {
-    event.preventDefault();
-    // console.log(date)
-    let meteo = [];
-    let inputCityName = document.getElementById("city").value.toLowerCase();
-    let eltSelect = document.querySelector("select");
-    ocd(inputCityName).then((data) => {
-      for (let i = 0; i < data.results.length; i++) {
-        meteo.push(data.results[0].geometry);
-        opw(meteo, eltSelect.selectedIndex);
-        if (document.querySelectorAll("img").length > 0) {
-          document.querySelector("#container").innerHTML = "";
-        }
-      }
+// temps - heure
+const time = (timeNow, timeset, timesun) => {
+  if (timeNow >= timeset || timeNow <= timesun) {
+    document.querySelector("body").style.backgroundColor = "#07406b";
+    document.querySelectorAll("*").forEach((node) => {
+      node.style.color = "#fff";
     });
-  });
+    document.querySelectorAll("img").forEach((img) => {
+      img.style.filter = "invert(1)";
+    });
+  } else {
+    document.querySelectorAll("*").forEach((node) => {
+      node.style.color = "#000";
+    });
+    document.querySelector("body").style.backgroundColor = "#bcd4e6";
+  }
+};
+
+// evenement
+city.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const cityName = nameOfCity.value;
+  let count = 0;
+  openCageData(cityName)
+    .then((data) => {
+      return data.results[0].geometry;
+    })
+    .then((meteo) => {
+      return openWeatherMap(meteo);
+    })
+    .then((data) => {
+      if (document.querySelectorAll("img").length > 0) {
+        document.querySelector("#container").innerHTML = "";
+      }
+      let hour = data.list[0].dt_txt.split(" ")[1];
+      data.list.forEach((element) => {
+        if (
+          element.dt_txt.split(hour).length > 1 &&
+          count < eltSelect.selectedIndex
+        ) {
+          let idOfMeteo = element.weather[0].id;
+          count++;
+          getWeather(idOfMeteo, element.dt);
+        }
+      });
+      let localTime = Math.floor(
+        (Date.now() + data.city.timezone * 1000) / 1000
+      );
+      let timeDec = new Date(localTime * 1000).getHours() - 2;
+      let sunsetDec = new Date(data.city.sunset * 1000).getHours();
+      let sunriseDec = new Date(data.city.sunrise * 1000).getHours();
+      time(timeDec, sunsetDec, sunriseDec);
+    });
 });
